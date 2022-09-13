5cc00e9b5036d5367b7a42d12f2c0657ee0baac5
AdH01C <adrianhengyl@gmail.com>
2022-09-13 18:19:07 +0800

    Start of project
    Signed-off-by: AdH01C <adrianhengyl@gmail.com>

--- src/main/java/com/csd/csd/CsdApplication.java
+++ src/main/java/com/csd/csd/CsdApplication.java
@@ -3,12 +3,19 @@ package com.csd.csd;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@RestController
 public class CsdApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(CsdApplication.class, args);
 	}
+	@GetMapping(value = "/some/path/test")
+	public String index(){
+		return "index";
+	}
+
 
 }
