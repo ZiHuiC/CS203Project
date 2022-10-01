60f01ccd636335b4ae15d91a438fa4f964d6c6a0
Ryan Peh <ryan.peh.2021@scis.smu.edu.sg>
2022-10-01 17:33:00 +0800

    list repo

--- src/main/java/com/csd/listing/ListingRepository.java
+++ src/main/java/com/csd/listing/ListingRepository.java
@@ -1,7 +1,9 @@
 package com.csd.listing;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ListingRepository extends JpaRepository<Listing, Long> {
 
     Listing findListingById(Long id);
