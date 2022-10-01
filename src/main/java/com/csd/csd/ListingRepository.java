315bc47f1798b9bf768b4624735469e9c6e914c2
Ryan Peh <ryan.peh.2021@scis.smu.edu.sg>
2022-10-01 12:18:17 +0800

    Revert listing repo

--- src/main/java/com/csd/csd/ListingRepository.java
+++ src/main/java/com/csd/csd/ListingRepository.java
@@ -1,10 +1,8 @@
 package com.csd.csd;
 
-import java.util.Optional;
-
 import org.springframework.data.repository.CrudRepository;
 
 public interface ListingRepository extends CrudRepository<Listing, Integer> {
 
-    Optional<Listing> findListingById(Integer id);
+    Listing findListingById(Integer id);
 }
